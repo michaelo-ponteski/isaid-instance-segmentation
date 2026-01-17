fix gradient explosion

models/maskrcnn_model.py
@@ -116,10 +116,10 @@ class MaskRCNNTrainer:
             # Backward pass
             self.optimizer.zero_grad()
             losses.backward()
-            
+
             # Gradient clipping to prevent explosion
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
-            
+
             self.optimizer.step()
 
             total_loss += losses.item()

utils/overfit_test.py
@@ -69,10 +69,10 @@ def overfit_single_image_test(model, dataset, idx=0, num_epochs=100, device="cud
         # Backward pass
         optimizer.zero_grad()
         losses.backward()
-        
+
         # Gradient clipping to prevent explosion
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        
+
         optimizer.step()
 
         losses_history.append(losses.item())